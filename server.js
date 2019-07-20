6a857a56172f78e0c406552176d19f2367e2952c
janeweix <1433987638@qq.com>
2019-07-20 09:45:15 +0800

    update

--- server.js
+++ server.js
@@ -1,40 +1,78 @@
-// 引入相应模块
-var http = require('http'),
-	 url = require('url'),
-	path = require('path'),
-	  fs = require('fs');
-	  
-var port = process.argv[2] || 8888;
+const path = require('path');
+// 构建服务器架构，配置好art-template、路由
+const Koa = require('koa');
+const fs = require('fs');
 
-var types = {
-	'mp3': 'audio/mpeg',
-	'html': 'text/html',
-	'js': 'application/javascript'
-},
-site = 'http://localhost:' + port;
+// 处理请求体数据
 
-http.createServer(function (request, response) {
-    var uri = url.parse(request.url).pathname,
-    filename = path.join(__dirname, uri);
-    
-    fs.exists(filename, function (exists) {
-       if (!exists) {
-           response.writeHead(404, {'Content-Type': 'text/plain', 'X-my-param':'zcyue'});
-           response.write('404 Not Found\n');
-           response.end();
-           return;
-       }
+let app = new Koa();
+// const cors = require('koa2-cors');
+const Router = require('koa-router');
+const router = new Router();
+// 处理文字和文件的请求体数据
+const formidable = require('koa-formidable');
 
-       if(!fs.lstatSync(filename).isDirectory()) {
-           var type = filename.split('.');
-           type = type[type.length - 1];
-           response.writeHead(200, { 'Content-Type': types[type] + '; charset=utf-8' });
-           fs.createReadStream(filename).pipe(response);
-        } else {
-           response.writeHead(301, {'Location': site + '/www/app.html' });
-           response.end();
-        }
+// var corsOptions = {
+//     origin: 'http://localhost:8070/',
+//     credentials: true,
+//     maxAge: '1728000'
+//     // 这一项是为了跨域专门设置的
+// };
+// app.use(cors(corsOptions));
+router.post('/upload', async(ctx, next) => {
+    console.log('上传成功');
+    ctx.body = 'ok';
+})
+.get('/', async ctx => {
+    // console.log(ctx.cookies.get('view'));
+    // const n = Number(ctx.cookies.get('view') || 0) + 1;
+    // ctx.cookies.set('view', n, {
+    //     httpOnly: false, // 是否只用于http请求中获取
+    // });
+    // ctx.cookies.set('name', 'tobi', {httpOnly: false});
+    ctx.body = {
+        token: 'abc',
+        msg: 'ok'
+    };
+})
+.post('/add', async ctx => {
+    ctx.req.on('data', data => {
+        console.log(data);
+        console.log(data.toString());
     });
-}).listen(parseInt(port, 10));
+    ctx.body = 'post ok';
+})
 
-console.log('Static file server running at\n => ' + site + '/\nCTRL + C to shutdown');
+// 对文件的操作
+app.use(async(ctx, next) => {
+    var formidable = require('formidable');
+    var form = new formidable.IncomingForm();
+    form.uploadDir = path.resolve('./');
+    form.parse(ctx.req, function(err, fields, files) {
+        console.log(files);
+    });
+    form.onPart = function(part) {
+        part.addListener('data', function(data) {
+            require('fs').appendFileSync('./4.mp3',data);
+        });
+    }
+    await next();
+});
+
+app.use(async(ctx, next) => {
+    console.log(ctx.request.header.origin);
+    ctx.response.set('Access-Control-Allow-Origin', '*');
+    // ctx.response.set('Access-Control-Allow-Origin', ctx.request.header.origin);
+    ctx.response.set("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    ctx.response.set("Access-Contron-Allow-Headers", 'token');
+    // ctx.response.set("Access-Control-Allow-Headers", true);
+    await next();
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+// 开启服务器
+app.listen(8888, () => {
+    console.log('服务器启动在8888端口');
+});
